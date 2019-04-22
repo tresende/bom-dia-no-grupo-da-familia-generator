b1b7f5d94016df7151de9b487268c11be40e7019
Thiago Resende <thiago.resende@dtidigital.com.br>
2019-04-22 14:18:44 -0300

    Código inicial

---
+++ index.js
@@ -0,0 +1,24 @@
+const google = require('googleapis').google
+const customSearch = google.customsearch('v1');
+const googleSearchCredentials = require('./credentials/google-search.json');
+
+async function fetchGoogleAndReturnImagesLinks(query) {
+    const response = await customSearch.cse.list({
+        auth: googleSearchCredentials.apiKey,
+        cx: googleSearchCredentials.searchEngineId,
+        q: query,
+        searchType: 'image',
+        num: 2
+    })
+
+    const imagesUrl = response.data.items.map((item) => {
+        return item.link
+    })
+
+    return imagesUrl
+}
+
+fetchGoogleAndReturnImagesLinks('landscape').then(data => console.log(data))
+
+var samuel = 'troxa';
+console.log(`samule é um ${samuel}`)
