764ae1ecfbd0d017f62ebac8384b2fea690432ba
tresende <thiago.gcresende@gmail.com>
2019-04-22 19:57:40 -0300

    setup devops

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-const google = require('googleapis').google
-const customSearch = google.customsearch('v1');
-const googleSearchCredentials = require('./credentials/google-search.json');
+const express = require("express");
+const cfenv = require("cfenv");
 
-async function fetchGoogleAndReturnImagesLinks(query) {
-    const response = await customSearch.cse.list({
-        auth: googleSearchCredentials.apiKey,
-        cx: googleSearchCredentials.searchEngineId,
-        q: query,
-        searchType: 'image',
-        num: 2
-    })
+var app = express();
 
-    const imagesUrl = response.data.items.map((item) => {
-        return item.link
-    })
+app.use(express.static(__dirname + "/public"));
+var appEnv = cfenv.getAppEnv();
+app.listen(appEnv.port, "0.0.0.0", () => console.log(`server starting on ${appEnv.url}`));
+// const google = require('googleapis').google
+// const customSearch = google.customsearch('v1');
+// const googleSearchCredentials = require('./credentials/google-search.json');
 
-    return imagesUrl
-}
+// async function fetchGoogleAndReturnImagesLinks(query) {
+//     const response = await customSearch.cse.list({
+//         auth: googleSearchCredentials.apiKey,
+//         cx: googleSearchCredentials.searchEngineId,
+//         q: query,
+//         searchType: 'image',
+//         num: 2
+//     })
 
-fetchGoogleAndReturnImagesLinks('landscape').then(data => console.log(data))
+//     const imagesUrl = response.data.items.map((item) => {
+//         return item.link
+//     })
+
+//     return imagesUrl
+// }
+
+// fetchGoogleAndReturnImagesLinks('landscape').then(data => console.log(data))
