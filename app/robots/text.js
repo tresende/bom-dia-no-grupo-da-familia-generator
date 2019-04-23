703f14fca4d1fa053545b4d9b75df32fa4dfae35
tresende <thiago.gcresende@gmail.com>
2019-04-22 21:51:03 -0300

    main.jpg

--- app/robots/text.js
+++ app/robots/text.js
@@ -1,31 +1,35 @@
 const Jimp = require("jimp");
 const fs = require('fs');
 
-var imageCaption = 'Bom Dia!';
-
 module.exports = {
     writeOnImage: async () => {
         return await new Promise((resolve, reject) => {
             const path = './public/images/';
-            fs.readdir(path, (err, items) => {
-                items.map(file => {
-                    const fileName = path + file;
-                    Jimp.read(fileName)
-                        .then((image) => {
-                            image.resize(800, Jimp.AUTO)
-                                .quality(50)
-                                .write(fileName);
-                            return image;
-                        })
-                        // .then((image) => {
-                        //     loadedImage = image;
-                        //     return Jimp.loadFont(Jimp.FONT_SANS_128_WHITE);
-                        // })
-                        // .then((font) => {
-                        //     loadedImage.print(font, 128, 10, imageCaption)
-                        //         .write(fileName);
-                        // })
-                        .then(() => resolve())
+            Jimp.read(path + 'main.png').then(src => {
+                fs.readdir(path, (err, items) => {
+                    items.map((file, index) => {
+                        const fileName = path + file;
+                        if (file == 'main.png' )
+                            return;
+                        Jimp.read(fileName)
+                            .then((image) => {
+                                image.resize(800, Jimp.AUTO)
+                                    .quality(50)
+                                    .write(fileName);
+                                return image;
+                            })
+                            .then((image) => {
+                                image.composite(src, 0, 200)
+                                    .write(fileName);
+                                return image;
+                            })
+                            .then(() => {
+                                //DS store no mac
+                                if (index == items.length - 1) {
+                                    resolve()
+                                }
+                            })
+                    });
                 });
             });
         });
