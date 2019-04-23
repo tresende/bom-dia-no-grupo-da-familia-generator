61b4faf68427198b29351ea47e66614ca9c49fe2
tresende <thiago.gcresende@gmail.com>
2019-04-22 22:21:41 -0300

    try catch em tudo

--- app/robots/images.js
+++ app/robots/images.js
@@ -5,29 +5,35 @@ const customSearch = google.customsearch('v1');
 module.exports = {
     getImages: async (query) => {
         return await new Promise((resolve, reject) => {
-            customSearch.cse.list({
-                auth: process.env.apiKey,
-                cx: process.env.searchEngineId,
-                q: query,
-                searchType: 'image',
-                num: process.env.images
-            }).then(response => {
-                response.data.items.map((item, index) => {
-                    const options = {
-                        url: item.link,
-                        dest: `./public/images/${index}.png`
-                    }
-                    download.image(options)
-                        .then(({ filename, image }) => {
-                            if (index == response.data.items.length - 1) {
-                                resolve();
-                            }
-                        })
-                        .catch((err) => {
-                            console.error(err)
-                        })
+            try {
+                customSearch.cse.list({
+                    auth: process.env.apiKey,
+                    cx: process.env.searchEngineId,
+                    q: query,
+                    searchType: 'image',
+                    num: process.env.images
+                }).then(response => {
+                    response.data.items.map((item, index) => {
+                        const options = {
+                            url: item.link,
+                            dest: `./public/images/${index}.png`
+                        }
+                        download.image(options)
+                            .then(({ filename, image }) => {
+                                if (index == response.data.items.length - 1) {
+                                    resolve();
+                                }
+                            })
+                            .catch((err) => {
+                                console.error(err)
+                            })
+                    });
+                }).catch(() => {
+                    resolve();
                 });
-            });
+            } catch (e) {
+                resolve();
+            }
         });
     }
 }
--- app/robots/text.js
+++ app/robots/text.js
@@ -4,34 +4,42 @@ const fs = require('fs');
 module.exports = {
     writeOnImage: async () => {
         return await new Promise((resolve, reject) => {
-            const path = './public/images/';
-            Jimp.read(path + 'main.png').then(src => {
-                fs.readdir(path, (err, items) => {
-                    items.map((file, index) => {
-                        const fileName = path + file;
-                        if (file == 'main.png' )
-                            return;
-                        Jimp.read(fileName)
-                            .then((image) => {
-                                image.resize(800, Jimp.AUTO)
-                                    .quality(50)
-                                    .write(fileName);
-                                return image;
-                            })
-                            .then((image) => {
-                                image.composite(src, 0, 200)
-                                    .write(fileName);
-                                return image;
-                            })
-                            .then(() => {
-                                //DS store no mac
-                                if (index == items.length - 1) {
+            try {
+                const path = './public/images/';
+                Jimp.read(path + 'main.png').then(src => {
+                    fs.readdir(path, (err, items) => {
+                        items.map((file, index) => {
+                            const fileName = path + file;
+                            if (file == 'main.png')
+                                return;
+                            Jimp.read(fileName)
+                                .then((image) => {
+                                    image.resize(800, Jimp.AUTO)
+                                        .quality(50)
+                                        .write(fileName);
+                                    return image;
+                                })
+                                .then((image) => {
+                                    image.composite(src, 0, 200)
+                                        .write(fileName);
+                                    return image;
+                                })
+                                .then(() => {
+                                    //DS store no mac
+                                    if (index == items.length - 1) {
+                                        resolve()
+                                    }
+                                }).catch((e) => {
                                     resolve()
-                                }
-                            })
+                                });
+                        });
                     });
+                }).catch((e) => {
+                    resolve()
                 });
-            });
+            } catch (error) {
+                resolve()
+            }
         });
     }
 }
